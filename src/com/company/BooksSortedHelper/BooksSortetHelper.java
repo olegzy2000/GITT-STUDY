82945f9b9fc5a40ed9516e7c0ed2592535941664
Oleg Zylevich <olegzy2000@gmail.com>
2020-07-18 00:38:09 +0300

    First commit(load all files)

--- src/com/company/BooksSortedHelper/BooksSortetHelper.java
+++ src/com/company/BooksSortedHelper/BooksSortetHelper.java
@@ -7,17 +7,11 @@ import java.util.Arrays;
 
 public class BooksSortetHelper {
     public static void printSortedByCost(ArrayList<Book>list){
-        for(int i=0;i<list.size();i++){
-            for(int j=i+1;j<list.size();j++){
-                if(list.get(i).getCost()<list.get(j).getCost()){
-                    Book book=list.get(i);
-                    list.set(i,list.get(j));
-                    list.set(j,book);
-                }
-            }
-        }
-        for(int i=0;i<list.size();i++){
-            System.out.println(list.get(i));
-        }
+        list.stream().sorted((z,c)-> {
+            Integer cost1=z.getCost();
+            Integer cost2=c.getCost();
+            return cost1.compareTo(cost2);
+        }).forEach(s->System.out.println(s));
     }
 }
+
