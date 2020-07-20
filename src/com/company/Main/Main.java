8651f901920e4f20555a22d99f40ca316555ea7b
Oleg Zylevich <olegzy2000@gmail.com>
2020-07-21 00:36:58 +0300

    First commit(load all files)

--- src/com/company/Main/Main.java
+++ src/com/company/Main/Main.java
@@ -1,6 +1,17 @@
 package com.company.Main;
 
+import com.company.Books.Book;
+import com.company.BooksSortedHelper.BooksSortetHelper;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Book> list=new ArrayList<Book>();
+        list.add(new Book("A",5,1));
+        list.add(new Book("B",3,2));
+        list.add(new Book("C",2,3));
+        BooksSortetHelper.printSortedByCost(list);
     }
 }
